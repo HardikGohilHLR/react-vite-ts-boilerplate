d8911a18939f22d466a49bdc419aea545f5b6865
Hardik Gohil <32103022+HardikGohilHLR@users.noreply.github.com>
2023-08-06 18:17:18 +0530

    home page added

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,14 +1,34 @@
 // Home
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 interface HomeProps { }
- 
+
 const Home: FC<HomeProps> = () => {
-    return ( 
+    return (
         <>
-            <h1>Home</h1>
+            <section className="section">
+
+                <div className="content">
+                    <h1>React.js + TypeScript Starter</h1>
+                    <p>A Starter for React.js, TypeScript with Redux and built with Vite</p>
+                    <Link to="https://github.com/HardikGohilHLR/react-vite-ts-boilerplate" target="_blank" className="btn">
+                        GitHub
+                    </Link>
+                </div>
+
+                <footer>
+                    <p>
+                        &copy; {new Date().getFullYear()} {" "}
+                        By <Link to="https://hardikgohilhlr.tech" target="_blank">
+                            Hardik Gohil
+                        </Link>
+                    </p>
+                </footer>
+
+            </section>
         </>
     );
 }
- 
+
 export default Home;
